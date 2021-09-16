585f126cca4d58d13f157d14a960e0db5cbd360d
sven-gerlach <svengerlach@me.com>
2021-09-16 14:05:13 -0400

    Set up user and superuser tests

--- app/tests/test_user_model.py
+++ app/tests/test_user_model.py
@@ -1,3 +1,31 @@
-# from django.test import TestCase
+"""
+Module for all User model related tests
+Source for pytes-django:
+https://djangostars.com/blog/django-pytest-testing/
+"""
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+import pytest
+
+# marking this module such that tests have access to the database
+pytestmark = pytest.mark.django_db
+
+
+def test_create_user():
+    """ensure a newly created user is in the database"""
+    User = get_user_model()
+    user = User.objects.create_user(email="email@me.com", password="foo")
+    assert User.objects.count() == 1
+    assert user.email == "email@me.com"
+    assert user.is_active is True
+
+
+def test_create_superuser():
+    """ensure a newly created superuser is in the database"""
+    User = get_user_model()
+    user = User.objects.create_superuser(email="super@user.de", password="foo")
+    assert user.email == "super@user.de"
+    assert user.is_active is True
+    assert user.is_staff is True
+    assert user.is_superuser is True
