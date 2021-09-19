1b50a00cad2520ae232e170a4768c08542ffa2d6
sven-gerlach <svengerlach@me.com>
2021-09-19 02:21:59 -0400

    Add random secret key as default to settings

--- secure_my_spot/settings.py
+++ secure_my_spot/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 import dj_database_url
 import django_heroku
@@ -53,7 +54,10 @@ else:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
+# if env variable is not available during deployment use random secret key
+# this work-around allows collectstatic to work during container build when secret key is not yet
+# available
+SECRET_KEY = os.environ.get("SECRET_KEY", default=get_random_secret_key())
 
 ALLOWED_HOSTS = ["*"]
 
