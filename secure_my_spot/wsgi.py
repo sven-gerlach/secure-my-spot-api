dfd410b420231498f154e491a56a3c18e32bf7dd
sven-gerlach <svengerlach@me.com>
2021-10-04 22:23:15 -0400

    Sort imports correctly

--- secure_my_spot/wsgi.py
+++ secure_my_spot/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+import os
+
 from django.conf import settings
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
-import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secure_my_spot.settings")
 
