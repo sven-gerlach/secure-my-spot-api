3e1fe5157feb01d08250392022c3bf12c514eef6
sven-gerlach <svengerlach@me.com>
2021-10-04 21:33:29 -0400

    Add whitenoise compression and caching back in

--- secure_my_spot/settings.py
+++ secure_my_spot/settings.py
@@ -205,7 +205,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # add compression and caching support for static files
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
