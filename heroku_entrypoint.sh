be9b33e1ed6b5253ddf0e80f2d205869cbb94035
sven-gerlach <svengerlach@me.com>
2021-10-13 16:15:30 -0400

    Modify heroku.yml's release and run commands

--- heroku_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-gunicorn secure_my_spot.wsgi:application --bind 0.0.0.0:$PORT
