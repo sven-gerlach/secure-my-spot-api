7dfbdb5c5024eccf99a0b782e9377a3844e40ec5
sven-gerlach <svengerlach@me.com>
2021-10-13 15:39:36 -0400

    Correct spelling error in heroku entrypoint file

---
+++ app/forms/landing_page_form.py
@@ -0,0 +1,7 @@
+"""
+Form for welcome / landing page to back-end.
+
+It should say a bit about the project and give an entry point to the authenticated admin end-point
+"""
+
+# todo: create landing page for back-end
