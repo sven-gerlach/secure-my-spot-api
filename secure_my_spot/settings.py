dcc7f467cca1e3d166321f1a4c053e416d3de150
sven-gerlach <svengerlach@me.com>
2022-01-11 14:22:43 -0500

    Revert cors policy to CLIENT_ORIGIN

--- secure_my_spot/settings.py
+++ secure_my_spot/settings.py
@@ -50,8 +50,7 @@ else:
     # Set debug to false
     DEBUG = False
     # Only allow the `CLIENT_ORIGIN` for CORS
-    # CORS_ALLOWED_ORIGINS = [os.getenv("CLIENT_ORIGIN")]
-    CORS_ALLOW_ALL_ORIGINS = False
+    CORS_ALLOWED_ORIGINS = [os.getenv("CLIENT_ORIGIN")]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
