a3bf77ffa010470a7a719b9c6cdfe611e07bc40a
sven-gerlach <svengerlach@me.com>
2022-01-11 13:06:25 -0500

    cors allow all origins = false

--- secure_my_spot/settings.py
+++ secure_my_spot/settings.py
@@ -51,7 +51,7 @@ else:
     DEBUG = False
     # Only allow the `CLIENT_ORIGIN` for CORS
     # CORS_ALLOWED_ORIGINS = [os.getenv("CLIENT_ORIGIN")]
-    CORS_ALLOW_ALL_ORIGINS = True
+    CORS_ALLOW_ALL_ORIGINS = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
