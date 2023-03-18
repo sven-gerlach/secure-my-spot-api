3595295b72e958ab997e5afc69786a90afa1d327
sven-gerlach <svengerlach@protonmail.com>
2023-03-17 21:27:46 -0400

    Amend run.sh

--- docker/proxy/run.sh
+++ docker/proxy/run.sh
@@ -17,11 +17,11 @@ export proxy_add_x_forwarded_for=\$proxy_add_x_forwarded_for
 export scheme=\$scheme
 
 # if fullchain.pem already exists then ssl has already been set up and nginx.tsl.conf ought to be run
-# otherwise run nginx.conf
+# otherwise run nginx.default.conf
 echo "checking for fullchain.pem"
 if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
   echo "No SSL certificate, enabling HTTP only..."
-  envsubst < /etc/nginx/nginx.conf > /etc/nginx/conf.d/default.conf
+  envsubst < /etc/nginx/nginx.default.conf > /etc/nginx/conf.d/default.conf
 else
   echo "SSL certificate exists, enabling HTTPS..."
   envsubst < /etc/nginx/nginx.ssl.conf > /etc/nginx/conf.d/default.conf
